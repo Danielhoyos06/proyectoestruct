5f9b11bb42549d1a83fef3deabb9f363176790fa
Danielhoyos06 <danielhoyos2006@gmail.com>
2025-10-15 17:22:06 -0500

    Add Fasta class for handling FASTA files

---
+++ Fasta.h
@@ -0,0 +1,20 @@
+#ifndef FASTA_H
+#define FASTA_H
+
+#include <string>
+#include <vector>
+#include "Secuencia.h"
+
+class Fasta {
+private:
+    std::vector<Secuencia> secuencias_; // Estructura lineal para almacenar las secuencias
+
+public:
+    // Carga un archivo FASTA y devuelve:
+    // - SIZE_MAX → error de E/S (no se puede leer)
+    // - 0        → archivo vacío (sin '>')
+    // - n >= 1   → n secuencias cargadas
+    size_t cargar(const std::string& nombre_archivo);
+};
+
+#endif // FASTADB_H
